b1986ac4ebe0cb1b511bc3d7cd806c7ec820bfb3
Jeffrey Hogan <jeff.hogan1@gmail.com>
2014-11-06 20:52:27 -0600

    Adding over some new URLs from provied examples in lecture 9

--- BookServer.py
+++ BookServer.py
@@ -1,6 +1,6 @@
 import flask
-import easypg
-
+from lib import easypg
+from lib import books, users
 
 easypg.config_name = 'bookserver'
 
@@ -22,6 +22,32 @@ def home():
     #    nproc, nser = proceedings.get_proceedings_stats(cur)
     #    stats = {'narticles': narts, 'nproceedings': nproc, 'nseries': nser}
     return flask.render_template('home.html')
+
+@app.route("/user/<uid>")
+def display_user(uid):
+    # Other user's profile page
+    raise NotImplementedError
+
+@app.route('/search')
+def get_search_results():
+    if 'q' in flask.request.args:
+        query = flask.request.args['q']
+    else:
+        flask.abort(400)
+    raise NotImplementedError
+
+@app.route('/articles/<aid>/comments/add', methods=['POST'])
+def add_comment(aid):
+    name = flask.request.form['name']
+    text = flask.request.form['comment']
+    raise NotImplementedError
+    app.logger.info('got comment from %s: %s', name, text)
+
+    with easypg.cursor() as cur:
+        articles.add_comment(cur, aid, name, text)
+    # redirect user back to article which will display comments
+    # always redirect after a POST
+    return flask.redirect('/articles/' + aid)
 
 
 if __name__ == "__main__":
