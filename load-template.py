e6e16a9acaeb9ca037f5fcd4f646313594f968f1
Jeffrey Hogan <jeff.hogan1@gmail.com>
2014-11-22 20:25:57 -0600

    Added more robust logging to parser. Included subject -> subject mapping

--- load-template.py
+++ load-template.py
@@ -7,26 +7,58 @@ import re
 
 easypg.config_name = 'bookserver'
 
+
+# Count the occurrences of any given key for a dataset
 author_keys = {'name_too_long':0}
 work_keys = {'title_too_long':0}
 book_keys = {'title_too_long':0}
 
-LOOP_LIMIT = False
+# Parse smaller chunks of data during some troubleshooting
+IS_LOOP_LIMIT = False
+LOOP_LIMIT = 50
+loop_counter = 0
+
+LOGGING = False
 
 log_file = open('load-template.log', 'w')
-
+error_log = open('template_error_log', 'w')
 
 author_ids = {}
 work_ids = {}
+subject_ids = {}
 
 # covers mapped to book_core.core_id for potential later use
 work_covers = {}
+
+def print_log_entry(log_file, message):
+  if LOGGING:
+    print >> log_file, message
+    
+def print_key_occurrences(heading, output, sorted_keys):
+    max_occurrences = sorted_author_keys[0][1]
+    display_cutoff = max_occurrences / 2
+    print >> output, "####%s Occurrences (>%s)" % (heading,display_cutoff)
+    print >> output, "| Key | # |"
+    print >> output, "| ---- | ---- |"
+    for x in sorted_keys:
+        if x[1] > display_cutoff:
+            print >> output, "| %s | %s |" % (x[0], x[1])
+    print >> output, "\n"
+
+    print >> output, "####%s Occurrences (all)" % (heading)
+    print >> output, "| Key | # |"
+    print >> output, "| ---- | ---- |"
+    for x in sorted_keys:
+        print >> output, "| %s | %s |" % (x[0], x[1])
+    print >> output, "\n"
+
+
 with open('data/sample-data/authors.json') as af:
-    count = 1
+    loop_counter = 0
     for line in af:
-        if LOOP_LIMIT and (count > 1):
+        if IS_LOOP_LIMIT and (loop_counter > LOOP_LIMIT):
             break
-        count += 1
+        loop_counter += 1
         author = json.loads(line.strip())
         author_key = author['key']
         for key in author:
@@ -48,17 +80,18 @@ with open('data/sample-data/authors.json') as af:
             try:
                 author['name'] = author['name'].strip().encode('ascii', 'xmlcharrefreplace')
             except KeyError:
-                print >> log_file, "No name for this author entry! Continuing..."
+                print_log_entry(error_log,"No name for this author entry (%s) ! Continuing..." % loop_counter)
                 continue
 
 
             if len(author['name']) > 255:
-                print >> log_file, "This author's name (%s) is too long, not importing it for the moment! Continuing..." % author['name']
+                print_log_entry(error_log,"This author's name (%s) is too long, not importing it for the moment! Continuing..." % author['name'])
+                author_keys['name_too_long'] += 1
                 continue
             try:
                 author['personal_name'] = author['personal_name'].strip().encode('ascii', 'xmlcharrefreplace')
                 if len(author['personal_name']) > 255:
-                    print >> log_file, "This author's key (%s) is too long, not importing it for the moment! Continuing..." % author['key']
+                    print_log_entry(error_log,"This author's personal_name (%s) is too long, not importing it for the moment! Continuing..." % author['key'])
                     continue
             except KeyError:
                 author['personal_name'] = ''
@@ -73,18 +106,17 @@ with open('data/sample-data/authors.json') as af:
             ''', (author['name'], author['personal_name']))
             if cur.rowcount == 1:
                 author_ids[author['key']] = cur.fetchone()[0]
-                print >> log_file, "Inserted author name  %s along with alias %s, represented by author_id: %s." % (author['name'], author['personal_name'], author_ids[author['key']])
-
-            print >> log_file, "-----------------------\n\n"
+                print_log_entry(log_file,"Inserted author name  %s along with alias %s, represented by author_id: %s." % (author['name'], author['personal_name'], author_ids[author['key']]))
 
 
+book_covers = open('cover_ids.json', 'w')
 with open('data/sample-data/works.json') as af:
-    book_covers = open('cover_ids.json', 'w')
-    count = 1
+
+    loop_counter = 0
     for line in af:
-        if LOOP_LIMIT and (count > 1):
+        if IS_LOOP_LIMIT and (loop_counter > LOOP_LIMIT):
             break
-        count += 1
+        loop_counter += 1
         work = json.loads(line.strip())
         work_key = work['key']
         #print 'found work', work_key
@@ -101,22 +133,18 @@ with open('data/sample-data/works.json') as af:
 
             except KeyError:
                 work_keys[key.strip().encode('ascii', 'xmlcharrefreplace')] = 1
-            # first clean up book title
-        try:
-            work['title'] = work['title'].strip().encode('ascii', 'xmlcharrefreplace')
-        except KeyError:
-            print >> log_file, "No title for this book work! Continuing..."
-            continue
-        with easypg.cursor() as cur:
-            # first clean up book title
+
+            # first clean up work title
             try:
                 work['title'] = work['title'].strip().encode('ascii', 'xmlcharrefreplace')
             except KeyError:
-                print >> log_file, "No title for this book entry! Continuing..."
+                print_log_entry(error_log,"No title for this work (%s)! Continuing..." % loop_counter)
                 continue
+        with easypg.cursor() as cur:
 
             if len(work['title']) > 250:
-                print >> log_file, "This work's title (%s) is too long, not importing it for the moment! Continuing..." % work['title']
+                work_keys['title_too_long'] += 1
+                print_log_entry(error_log,"This work's title (%s) is too long, not importing it for the moment! Continuing..." % work['title'])
                 continue
 
             try:
@@ -124,7 +152,9 @@ with open('data/sample-data/works.json') as af:
             except KeyError:
                 work['description'] = ""
             except AttributeError:
-                # print to error log later
+                # print to error log later.
+                work['description'] = ""
+                print_log_entry(error_log,"Can't use description, AttributeError (%s)." % loop_counter)
                 pass
 
             # now check to see if we have an existing "book_core" entry
@@ -135,10 +165,10 @@ with open('data/sample-data/works.json') as af:
                 WHERE book_title = %s
             ''', (work['title'],))
 
-            if (cur.rowcount != 1):
-                print >> log_file, "No book core entry found, adding one now..."
+            if cur.rowcount != 1:
+                print_log_entry(log_file,"No book core entry found, adding one now...")
                 #too many, or no, matching book_core entries found -> make a new one
-                print >> log_file, "Inserting work / book core: %s." % (work['title'])
+                print_log_entry(log_file,"Inserting work / book core: %s :: %s." % (work['title'], loop_counter))
                 cur.execute('''
                   INSERT INTO book_core (book_title, book_description, edition)
                   VALUES(%s, %s, %s)
@@ -162,6 +192,7 @@ with open('data/sample-data/works.json') as af:
                 work_covers[book_core_id] = work['covers']
             except KeyError:
                 # No covers here
+                print_log_entry(error_log,"No covers found for %s" % loop_counter)
                 pass
 
             # add author relationships if found
@@ -170,7 +201,7 @@ with open('data/sample-data/works.json') as af:
                     author = author[key]
                     position = 1
                     try:
-                        print >> log_file, author
+                        print_log_entry(log_file,author)
                         author_id = author_ids[author]
                         cur.execute('''
                         INSERT INTO authorship (core_id, author_id, position)
@@ -179,22 +210,21 @@ with open('data/sample-data/works.json') as af:
                         ''', (book_core_id, author_id, position))
                         position += 1
                     except KeyError:
-                        print >> log_file, "Couldn't find an author_id entry for author key: %s..." % author
+                        print_log_entry(error_log,"Couldn't find an author_id entry for author key: %s..." % author)
             except KeyError:
-                print >> log_file, "No authors found!"
+                print_log_entry(error_log,"No authors found (%s)!" % loop_counter)
 
-            # print >> log_file, "-----------------------\n\n"
-    json.dump(work_covers, book_covers)
-    book_covers.close()
+json.dump(work_covers, book_covers)
+book_covers.close()
 
 
 
 with open('data/sample-data/books.json') as af:
-    count = 1
+    loop_counter = 0
     for line in af:
-        if LOOP_LIMIT and (count > 2000):
+        if IS_LOOP_LIMIT and (loop_counter > LOOP_LIMIT):
             break
-        count += 1
+        loop_counter += 1
         book = json.loads(line.strip())
         book_key = book['key']
         for key in book:
@@ -218,20 +248,21 @@ with open('data/sample-data/books.json') as af:
                 book['title'] = book['title'].strip().encode('ascii', 'xmlcharrefreplace')
             except KeyError:
                 # should be output to error log 
-                #print "No title for this book entry! Continuing..."
+                print_log_entry(error_log,"No title for this book entry (%s) ! Continuing..." % loop_counter)
                 continue
 
 
             if len(book['title']) > 250:
                 # log this latter
-                #print "This book's title (%s) is too long, not importing it for the moment! Continuing..." % book['title']
+                book_keys['title_too_long'] += 1
+                print_log_entry(error_log,"This book's title (%s) is too long, not importing it for the moment! Continuing..." % book['title'])
                 continue
 
             # now check to see if we have an existing "book_core" entry
-            print >> log_file, "Checking to see if a book core entry exists for %s..." % book['title']
+            print_log_entry(log_file,"Checking to see if a book core entry exists for %s..." % book['title'])
             try:
                 book_core_id = work_ids[book['work']]
-                print >> log_file, "Book core ID obtained: %s!" % book_core_id
+                print_log_entry(log_file,"Book core ID obtained: %s!" % book_core_id)
             except KeyError:
 
                 # cur.execute('''
@@ -241,7 +272,7 @@ with open('data/sample-data/books.json') as af:
                 # ''', (book['title'],))
 
                 # if (cur.rowcount != 1):
-                print >> log_file, "No book core entry found (work_ids[%s]), adding one now..." % book['work']
+                print_log_entry(error_log,"No book core entry found (work_ids[%s]), adding one now..." % book['work'])
                 #too many, or no, matching book_core entries found -> make a new one
                 cur.execute('''
                   INSERT INTO book_core (book_title, book_description, edition)
@@ -262,7 +293,7 @@ with open('data/sample-data/books.json') as af:
                 try:
                     book['isbn_10']
                 except KeyError:
-                    print >> log_file, "%s has no ISBN!" % book['title']
+                    print_log_entry(error_log,"%s has no ISBN!" % book['title'])
                     book_isbn = []
                 else:
                     book_isbn = book['isbn_10']
@@ -273,7 +304,7 @@ with open('data/sample-data/books.json') as af:
                 book['number_of_pages']
             except KeyError:
                 page_count = None
-                print >> log_file, "%s has no page count!" % book['title']
+                print_log_entry(error_log,"%s has no page count!" % book['title'])
             else:
                 page_count = book['number_of_pages']
 
@@ -285,46 +316,73 @@ with open('data/sample-data/books.json') as af:
                 book_type = book['physical_format']
 
             try:
-                print >> log_file, "Publish date is: %s." % book['publish_date']
+                print_log_entry(log_file,"Publish date is: %s." % book['publish_date'])
                 original_date = book['publish_date']
             except KeyError:
-                print >> log_file, "%s has no publication date!" % book['title']
+                print_log_entry(error_log,"%s has no publication date!" % book['title'])
                 publication_date = None
                 original_date = None
             except UnicodeEncodeError:
-                # log this later
+                print_log_entry(error_log,"UnicodeEncodeError for publish_date (%s)?" % loop_counter)
                 pass
             else:
                 if re.match('\w* \d{1,2}, \d{4}', book['publish_date']):
                     m = re.match('(\w* \d{1,2}, \d{4})', book['publish_date'])
-                    # print m.groups()
                     publication_date = m.groups()[0]
                 elif re.match('\w* \d{4}', book['publish_date']):
                     m = re.match('(\w*) (\d{4})', book['publish_date'])
-                    # print m.groups()
                     publication_date = "%s 01, %s" % (m.groups()[0], m.groups()[1])
                 elif re.match('\d{4}', book['publish_date']):
                     m = re.match('(\d{4})', book['publish_date'])
-                    # print m.groups()
                     publication_date = "January 01, %s" % m.groups()[0]
                 else :
                     publication_date = None
-            print >> log_file, "Parsed publication_date is: %s (Original date: %s)." % (publication_date, original_date)
+            print_log_entry(log_file,"Parsed publication_date is: %s (Original date: %s)." % (publication_date, original_date))
             # was getting output in this formatting "[u'9780110827667']" slicing off the excess
             # until I find whats going on
             # book_isbn = book_isbn[2:-2]
             for isbn in book_isbn:
                 isbn = ''.join(x for x in isbn if x.isdigit())
-                print >> log_file, "Inserting book title %s along with core_id %s (ISBN: %s - Date: %s)." % (book['title'], book_core_id, isbn, publication_date)
+                print_log_entry(log_file,"Inserting book title %s along with core_id %s (ISBN: %s - Date: %s)." % (book['title'], book_core_id, isbn, publication_date))
                 cur.execute('''
                     INSERT INTO books (core_id, publication_date, isbn, book_type, page_count)
                     VALUES(%s, %s, %s, %s, %s)
                 ''', (book_core_id, publication_date, isbn, book_type, page_count))
 
+            try:
+                for subject in book['subjects']:
+                    subject = subject.strip().encode('ascii', 'xmlcharrefreplace')
+                                        #print "Checking to see if a subject entry exists for %s..." % tag
+                    cur.execute('''
+                        SELECT subject_id
+                        FROM subject_genre
+                        WHERE subject_name = %s
+                    ''', (subject,))
+
+                    if cur.rowcount != 1:
+                        print_log_entry(log_file,"No subject entry found, adding one now(%s)..." % loop_counter)
+                        #too many, or no, matching book_core entries found -> make a new one
+                        print_log_entry(log_file,"Inserting subject: %s :: %s." % (subject, loop_counter))
+                        cur.execute('''
+                          INSERT INTO subject_genre (subject_name)
+                          VALUES(%s)
+                          RETURNING subject_id
+                        ''', (subject,))
+
+                    # Retrieve book_core_id for book insertion
+                    subject_id = cur.fetchone()[0]
+
+                    cur.execute('''
+                      INSERT INTO book_categorization (core_id, subject_id)
+                      VALUES(%s, %s)
+                    ''', (book_core_id,subject_id))
+            except KeyError:
+                print_log_entry(error_log,"Unable to access book['subjects'] for tag creation (%s)..." % loop_counter)
+
+
             # add author relationships if found
             position = 1
             try:
-
                 for author in book['authors']:
                     position += 1
                     try:
@@ -336,38 +394,21 @@ with open('data/sample-data/books.json') as af:
                         ''', (book_core_id, author_id, position))
                         position += 1
                     except KeyError:
-                        print >> log_file, "KeyError: Couldn't find an author_id entry for author key: %s..." % author
+                        print_log_entry(error_log,"KeyError: Couldn't find an author_id entry for author key: %s..." % author)
                     except TypeError:
-                        print >> log_file, "TypeError: Couldn't find an author_id entry for author key: %s..." % author
+                        print_log_entry(error_log,"TypeError: Couldn't find an author_id entry for author key: %s..." % author)
             except KeyError:
-                print >> log_file, "No authors found!"
+                print_log_entry(error_log,"No authors found!")
 
-            print >> log_file, "-----------------------\n\n"
+sorted_author_keys = sorted(author_keys.items(), key=operator.itemgetter(1), reverse=True)
+sorted_work_keys = sorted(work_keys.items(), key=operator.itemgetter(1), reverse=True)
+sorted_book_keys = sorted(book_keys.items(), key=operator.itemgetter(1), reverse=True)
 
+markdown_file = open('load-template-results.md', 'w')
+print_key_occurrences("Author Keys", markdown_file, sorted_author_keys)
+print_key_occurrences("Work Keys", markdown_file, sorted_work_keys)
+print_key_occurrences("Book Keys", markdown_file, sorted_book_keys)
 
-
-sorted_author_keys = sorted(author_keys.items(), key=operator.itemgetter(1))
-sorted_work_keys = sorted(work_keys.items(), key=operator.itemgetter(1))
-sorted_book_keys = sorted(book_keys.items(), key=operator.itemgetter(1))
-
-print "***** Author Key Occurances"
-print "| Key | # |"
-for x in sorted_author_keys:
-    print "| %s | %s |" % (x[0], x[1])
-print "-----------------------\n\n"
-
-
-print "***** Work Key Occurances"
-print "| Key | # |"
-for x in sorted_work_keys:
-    print "| %s | %s |" % (x[0], x[1])
-print "-----------------------\n\n"
-
-
-print "***** Book Key Occurances"
-print "| Key | # |"
-for x in sorted_book_keys:
-    print "| %s | %s |" % (x[0], x[1])
-print "-----------------------\n\n"
-
+markdown_file.close()
 log_file.close()
+error_log.close()
